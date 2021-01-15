9316cd62bc73a49ec7b6c698a39745b70c1f386f
Thomas Bolon <thomas.bolon@gmail.com>
2021-01-15 17:48:45 +0100

    misc

--- src/FlipLeaf/FlipLeafProgram.cs
+++ src/FlipLeaf/FlipLeafProgram.cs
@@ -21,12 +21,25 @@ namespace FlipLeaf
         public static void Main<TStartup>(string[] args)
             where TStartup : class
         {
-            Host
-                .CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((hostingContext, config) => config.AddEnvironmentVariables())
-                .ConfigureWebHostDefaults(builder => builder.UseWebRoot(@".static").UseStartup<TStartup>())
-                .Build()
-                .Run();
+            var startConsole = false;
+            if (args != null && args.Length != 0 && args[0] == "console")
+            {
+                startConsole = true;
+            }
+
+            if (!startConsole)
+            {
+                Host
+                    .CreateDefaultBuilder(args)
+                    .ConfigureAppConfiguration((hostingContext, config) => config.AddEnvironmentVariables())
+                    .ConfigureWebHostDefaults(builder => builder.UseWebRoot(@".static").UseStartup<TStartup>())
+                    .Build()
+                    .Run();
+
+                return;
+            }
+
+            // TODO : start console version
         }
     }
 }
